d2b1b0d009ab107e38f6a720b8a8a968474aa312
Nazar0vIvan <vano888888@gmail.com>
2023-06-06 19:39:11 +0300

    add JS Basics t1

---
+++ JS Basics/task1/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", "");
+
+let personalMoviewDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+
+const lastMovie1 = prompt("Один из последних просмотренных фильмов?",""),
+      raiting1 = prompt("На сколько оцените его",""),
+      lastMovie2 = prompt("Один из последних просмотренных фильмов?",""),
+      raiting2 = prompt("На сколько оцените его","");
+
+
+personalMoviewDB.movies[lastMovie1] = raiting1;
+personalMoviewDB.movies[lastMovie2] = raiting2;
+
+console.log(personalMoviewDB);
