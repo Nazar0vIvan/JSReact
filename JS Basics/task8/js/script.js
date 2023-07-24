6ef83a5a51af080c78313e234ff8cc127013f7e9
BormachBoroda <vano888888@gmail.com>
2023-07-24 17:52:56 +0300

    add JS Basics task8

--- JS Basics/task8/js/script.js
+++ JS Basics/task8/js/script.js
@@ -39,18 +39,26 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   // TIMER
-  const deadline = "2023-07-30";
+  const deadline = "2019-07-30";
 
   function appendZero(num) {
     return (num >= 0 && num < 10) ? `0${num}` : num;
   }
 
   function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
     const total = Date.parse(endtime) - Date.parse(new Date());
-    const days = Math.floor(total / (1000 * 60 * 60 * 24)),
-          hours = Math.floor((total / (1000 * 60 * 60)) % 24),
-          minutes = Math.floor((total / (1000 * 60)) % 60),
-          seconds = Math.floor((total / 1000) % 60);
+
+    if (total <= 0) {
+      days = hours = minutes = seconds = 0;
+    }
+    else {
+      days = Math.floor(total / (1000 * 60 * 60 * 24));
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((total / (1000 * 60)) % 60);
+      seconds = Math.floor((total / 1000) % 60);
+    }
+
     return { total, days, hours, minutes, seconds };
   }
 
@@ -72,7 +80,7 @@ window.addEventListener("DOMContentLoaded", () => {
       minutes.innerHTML = appendZero(remainingTime.minutes);
       seconds.innerHTML = appendZero(remainingTime.seconds);
 
-      if (t.total <= 0) clearInterval(timeInterval);
+      if (remainingTime.total <= 0) clearInterval(timeInterval);
     } 
   }
 
