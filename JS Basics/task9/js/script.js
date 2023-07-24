db5f381d332e86a07c2bb4a5440e7f8318181064
BormachBoroda <vano888888@gmail.com>
2023-07-24 18:40:52 +0300

    add JS Basics task9

---
+++ JS Basics/task9/js/script.js
@@ -0,0 +1,132 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  
+  // TABS
+  const tabsParent = document.querySelector(".tabheader__items"),
+        tabs = document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  // TIMER
+  const deadline = "2019-07-30";
+
+  function appendZero(num) {
+    return (num >= 0 && num < 10) ? `0${num}` : num;
+  }
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const total = Date.parse(endtime) - Date.parse(new Date());
+
+    if (total <= 0) {
+      days = hours = minutes = seconds = 0;
+    } else {
+      days = Math.floor(total / (1000 * 60 * 60 * 24));
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((total / (1000 * 60)) % 60);
+      seconds = Math.floor((total / 1000) % 60);
+    }
+
+    return { total, days, hours, minutes, seconds };
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days"),
+          hours = timer.querySelector("#hours"),
+          minutes = timer.querySelector("#minutes"),
+          seconds = timer.querySelector("#seconds"),
+          timeInterval = setInterval(updateClock, 1000);
+    
+    updateClock(); // начальная установка часов 
+
+    function updateClock() {
+      const remainingTime = getTimeRemaining(endtime);
+
+      days.innerHTML = appendZero(remainingTime.days);
+      hours.innerHTML = appendZero(remainingTime.hours);
+      minutes.innerHTML = appendZero(remainingTime.minutes);
+      seconds.innerHTML = appendZero(remainingTime.seconds);
+
+      if (remainingTime.total <= 0) clearInterval(timeInterval);
+    } 
+  }
+
+  setClock(".timer", deadline);
+
+  // MODAL
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+        modal = document.querySelector(".modal"),
+        modalCloseBtn = document.querySelector("[data-close]");
+
+  function openModal() {
+    modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  function closeModal() {
+    modal.classList.toggle("show");
+    document.body.style.overflow = "";
+  }
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    } 
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+  
+  window.addEventListener("scroll", showModalByScroll);
+});
