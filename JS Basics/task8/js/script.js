17a788729b938a3e1f698d2b0e1953c8ebbfff86
BormachBoroda <vano888888@gmail.com>
2023-07-24 17:21:02 +0300

    add JS Basics task8

---
+++ JS Basics/task8/js/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  
+  // TABS
+  const tabsParent = document.querySelector(".tabheader__items"),
+        tabs = document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  // TIMER
+  const deadline = "2023-07-30";
+
+  function appendZero(num) {
+    return (num >= 0 && num < 10) ? `0${num}` : num;
+  }
+
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(total / (1000 * 60 * 60 * 24)),
+          hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+          minutes = Math.floor((total / (1000 * 60)) % 60),
+          seconds = Math.floor((total / 1000) % 60);
+    return { total, days, hours, minutes, seconds };
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days"),
+          hours = timer.querySelector("#hours"),
+          minutes = timer.querySelector("#minutes"),
+          seconds = timer.querySelector("#seconds"),
+          timeInterval = setInterval(updateClock, 1000);
+    
+    updateClock(); // начальная установка часов 
+
+    function updateClock() {
+      const remainingTime = getTimeRemaining(endtime);
+
+      days.innerHTML = appendZero(remainingTime.days);
+      hours.innerHTML = appendZero(remainingTime.hours);
+      minutes.innerHTML = appendZero(remainingTime.minutes);
+      seconds.innerHTML = appendZero(remainingTime.seconds);
+
+      if (t.total <= 0) clearInterval(timeInterval);
+    } 
+  }
+
+  setClock(".timer", deadline);
+});
+
+
+
+
+
