c928248461eda41f2a84015b27e1e1a6ce4d1f2a
BormachBoroda <vano888888@gmail.com>
2023-10-11 18:31:00 +0300

    feat: add Additional JS Basics

---
+++ Additional JS Basics/task_002/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/*
+Оператор нулевого слияния (Nullish) - ??
+Спотыкается на всём КРОМЕ null или undefined (похож на ИЛИ)
+
+"+"
+1) Позволяет использовать обычные конвертируемые в false значения 
+   (кроме null или undefined) в качестве true в логических выражениях
+
+"-"
+1) ?? и && можно использовать в одном выражении только со скобками
+*/
+
+const box = document.querySelector('.box');
+
+const newHeight = 0; // 100;
+const newWidth = 0; // 400;
+
+/*
+  Теперь при передаче 0-ых размеров, будет 
+  выводится элемент с размерами 0х0, а не 200х200
+*/ 
+function changeParams(elem, h, w) {
+  // elem.style.height = `${h || 200}px`;
+  // elem.style.width = `${w || 200}px`;
+  elem.style.height = `${h ?? 200}px`;
+  elem.style.width = `${w ?? 200}px`;
+}
+
+changeParams(box, newHeight, newWidth);
