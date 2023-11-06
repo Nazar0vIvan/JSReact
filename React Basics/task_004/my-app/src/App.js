aba5cd1b6d770a3ff87f89883616730548e4d58d
Nazar0vIvan <vano888888@gmail.com>
2023-11-07 02:01:46 +0300

    feat: add React_Basics/task_004

---
+++ React Basics/task_004/my-app/src/App.js
@@ -0,0 +1,45 @@
+import { Component } from "react"
+import "./App.css";
+
+const Header = () => {
+  return <h2>Hello World</h2>;
+};
+
+/*
+const Field = () => {
+  const holder = "Enter here";
+  const styledField = {
+    width: "300px",
+  };
+  return <input placeholder={holder} type="text" style={styledField} />;
+};
+*/
+
+class Field extends Component {
+  render() {
+    const holder = "Enter here";
+    const styledField = {
+      width: "300px",
+    };
+    return <input placeholder={holder} type="text" style={styledField} />;
+  }
+}
+
+function Btn() {
+  const text = "Log in";
+  const logged = false;
+  return <button>{logged ? "Enter" : text}</button>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Field />
+      <Btn />
+    </div>
+  );
+}
+
+export { Header };
+export default App;
