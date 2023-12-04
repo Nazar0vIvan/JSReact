860a940df9a630ca45eb085eb8f84a10a95e1d9e
Nazar0vIvan <vano888888@gmail.com>
2023-12-05 01:46:03 +0300

    feat: add UseStateHook

--- web-dev-simplified/UseStateHook/src/App.jsx
+++ web-dev-simplified/UseStateHook/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
-function slowGetter() {
-  return "Kyle";
-}
-
 function App() {
-  const [name, setName] = useState(slowGetter());
+  const [name, setName] = useState("Kyle");
+  const [age, setAge] = useState(27);
 
   function handleClick() {
     setName("Sally");
+    setAge(age + 1);
   }
 
-  return <h1 onClick={handleClick}>Hi {name}</h1>;
+  return (
+    <h1 onClick={handleClick}>
+      Hi {name} {age}
+    </h1>
+  );
 }
 
 export default App;
