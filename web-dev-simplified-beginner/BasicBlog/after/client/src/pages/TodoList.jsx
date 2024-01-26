d469d1ea80fa85af26b0c18143dbb70b7c3c6763
BormachBoroda <vano888888@gmail.com>
2024-01-26 18:51:28 +0300

    feat: add web-dev-simplified-beginner BasicBlog

--- web-dev-simplified-beginner/BasicBlog/after/client/src/pages/TodoList.jsx
+++ web-dev-simplified-beginner/BasicBlog/after/client/src/pages/TodoList.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import { getTodos } from "../api/todos";
+import TodoItem from "../components/TodoItem";
 
 function TodoList() {
   const todos = useLoaderData();
@@ -8,12 +9,7 @@ function TodoList() {
       <h1 className="page-title">Todos</h1>
       <ul>
         {todos.map((todo) => (
-          <li
-            key={todo.id}
-            className={todo.completed ? "strike-through" : undefined}
-          >
-            {todo.title}
-          </li>
+          <TodoItem key={todo.id} {...todo} />
         ))}
       </ul>
     </>
--- web-dev-simplified-beginner/BasicBlog/after/client/src/router.jsx
+++ web-dev-simplified-beginner/BasicBlog/after/client/src/router.jsx
@@ -1,4 +1,4 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter, useRouteError } from "react-router-dom";
 import { userListRoute } from "./pages/UserList";
 import { postListRoute } from "./pages/PostList";
 import { todoListRoute } from "./pages/TodoList";
@@ -11,22 +11,44 @@ export const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <Navigate to="/posts" /> },
       {
-        path: "posts",
+        errorElement: <ErrorPage />,
         children: [
-          { index: true, ...postListRoute },
-          { path: ":postId", ...postRoute },
+          { index: true, element: <Navigate to="/posts" /> },
+          {
+            path: "posts",
+            children: [
+              { index: true, ...postListRoute },
+              { path: ":postId", ...postRoute },
+            ],
+          },
+          {
+            path: "users",
+            children: [
+              { index: true, ...userListRoute },
+              { path: ":userId", ...userRoute },
+            ],
+          },
+          { path: "todos", ...todoListRoute },
+          { path: "*", element: <h1>404 - Page Not Found</h1> },
         ],
       },
-      { 
-        path: "users",
-        children: [
-          { index: true, ...userListRoute },
-          { path: ":userId", ...userRoute },
-        ],
-      },
-      { path: "todos", ...todoListRoute },
     ],
   },
 ]);
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <>
+      <h1>Error - Something went wrong</h1>
+      {import.meta.env.MODE !== "production" && (
+        <>
+          <pre>{error.message}</pre>
+          <pre>{error.stack}</pre>
+        </>
+      )}
+    </>
+  );
+}
