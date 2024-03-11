45f6ab4e10307a41f43742e5b08da6b905b44137
Bormach_Boroda <vano888888@gmail.com>
2024-03-11 23:50:19 +0300

    chore: not essential changes

--- CST/src/components/TreeView.jsx
+++ CST/src/components/TreeView.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import ArrowIcon from "./arrow.svg";
+import FileIcon from "./file.svg";
 
 export function TreeView({ data }) {
   return (
@@ -14,15 +17,33 @@ function TreeNode({ node }) {
   const [isOpen, setIsOpen] = useState(false);
   const { children, label } = node;
 
+  function toggleOpen() {
+    setIsOpen(!isOpen);
+  }
+
+  function isChildren() {
+    return children != null;
+  }
+
   return (
-    <div className="tree-node">
-      <button
-        onClick={children != null ? () => setIsOpen(!isOpen) : null}
-        className="toggle-icon"
-      >
-        {children == null ? "*" : isOpen ? "⮝" : "⮟"}
-      </button>
-      <span>{label}</span>
+    <div className="tree-node-wrapper">
+      <div className="tree-node">
+        <div
+          className={`tree-node__mark ${
+            isOpen ? "tree-node__mark_active" : ""
+          }`}
+        ></div>
+        <Link
+          className="tree-node__link"
+          onClick={isChildren() ? toggleOpen : null}
+        >
+          <img
+            className="tree-node__icon"
+            src={isChildren() ? ArrowIcon : FileIcon}
+          ></img>
+          <div className="tree-node__text">{label}</div>
+        </Link>
+      </div>
       {isOpen && <TreeView data={children} />}
     </div>
   );
