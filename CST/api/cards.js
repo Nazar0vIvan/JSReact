db2965f48804a4d8e96658cb44eb3fd2ff47b968
BormachBoroda <vano888888@gmail.com>
2024-03-13 11:09:21 +0300

    feat: delete CST

--- CST/api/cards.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export async function getCards(options) {
-  return axios
-    .get("./api/db.json", options)
-    .then((response) => response.data.cards);
-}
--- CST/src/components/Contents.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TreeView } from "./TreeView";
-import { useLoaderData } from "react-router-dom";
-
-export function Contents() {
-  const contents = useLoaderData();
-  return <TreeView data={contents} />;
-}
