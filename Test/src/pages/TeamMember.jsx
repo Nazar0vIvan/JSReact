869e67d7419f84f1e49f5a071984d36a183a3108
Bormach_Boroda <vano888888@gmail.com>
2024-03-29 01:02:29 +0300

    chore: not essential changes

--- Test/src/pages/TeamMember.jsx
+++
@@ -1,8 +0,0 @@
-import { useLoaderData, useParams } from "react-router-dom";
-
-function TeamMember() {
-  const member = useLoaderData();
-  return <h1>Team Member - {member.name}</h1>;
-}
-
-export default TeamMember;
